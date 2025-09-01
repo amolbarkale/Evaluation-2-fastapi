dd4bed0b570949194beee999275612598ad68688
amolbarkale <barkaleamol@gmail.com>
2025-09-01 18:26:23 +0530

    uncomment endpoints for users router

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ app = FastAPI(title="Digital Wallet API Application")
 
 Base.metadata.create_all(bind=engine)
 
-app.include_router(transactions.router)
 app.include_router(transfers.router)
+app.include_router(transactions.router)
 app.include_router(users.router)
 app.include_router(wallet.router)
 
