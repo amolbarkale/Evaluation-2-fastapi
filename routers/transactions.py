b94459d789d6835cf4ac36fdcf159d328d35a1a7
amolbarkale <barkaleamol@gmail.com>
2025-09-01 18:59:48 +0530

    feat: add endpoint for transaction

--- routers/transactions.py
+++ routers/transactions.py
@@ -1,3 +1,13 @@
 from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+
+from .. import models, database_connection
 
 router = APIRouter(prefix="/transactions", tags=["transactions"])
+
+@router.get("/detail/{transaction_id}")
+async def get_transaction_detail(transaction_id: int, db: Session = Depends(database_connection.get_db)):
+    transaction = db.query(models.Transaction).filter(models.Transaction.id == transaction_id).first()
+    if not transaction:
+        raise HTTPException(status_code=404, detail="Transaction not found")
+    return transaction
