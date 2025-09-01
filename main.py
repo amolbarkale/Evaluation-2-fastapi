ba363ebe9a690715e7936ef53e40506eefa5899c
amolbarkale <barkaleamol@gmail.com>
2025-09-01 17:02:34 +0530

    feat: create entry file for fastapi

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
-from fastapi import FastAPI, HTTPException, status
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI()
+from . import models
+from .routers import transactions, transfers, users
+
+app = FastAPI(title="Digital Wallet API Application")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+models.Base.metadata.create_all(bind=models.engine)
+
+app.include_router(transactions.router)
+app.include_router(transfers.router)
+app.include_router(users.router)
 
 @app.get("/ping")
 async def ping():
